3f7a5a15c99ce39e151dc20d1b625d0d6bbd2589
Humberto Hernandez <hamburger45@gmail.com>
2019-11-01 21:43:12 -0700

    added JS start quiz and render question

--- app.js
+++ app.js
@@ -4,16 +4,43 @@ console.log('Script is connected!');
 // **hint check samples provided to see how they structuyred data 
 // ** else use and array object [{querstion: '?', answer: 'true || false'}]
 
+function hideQuestions(){
+    $('.questions').hide();
+};
+
 // Start the quiz 
 // function start quiz {
 //     listen for button click "start" and render quistions
 //  **Hint** try and use show and hide methods vs fully rendering 
 // }
+function startQuiz() {
+    // listen for button click #js-start-quiz and render quistions
+    $('#js-start-quiz').on('click', function(){
+        console.log('Start button clicked!')
+        // render questions
+        renderQuestion();
+        $('.questions').show();
+    });
+};
 
-// function render score board {
-//     display score board with current score
-//     **this may not be DRY but validate before removing!!
+// function render questions {
 // }
+function renderQuestion(){
+    let question = STORE.questions
+    [STORE.currentQuestion];
+    //     display questions
+    const quizQuestion = question.question;
+    console.log(question.question);
+    const questionDisplayed = $(`
+    <li>${question.options[0]}</li>
+    <li>${question.options[1]}</li>
+    <li>${question.options[2]}</li>
+    <li>${question.options[3]}</li>
+    `);
+    $('div.homePage').hide();
+    $('.currentQuestion').text(quizQuestion);
+    $('.displayQuestions').html(questionDisplayed);
+};
 
 // function keep score {
 //     update score board based on correct or wrong answers 
@@ -24,6 +51,11 @@ console.log('Script is connected!');
 //     listen for user answer 
 //     check if user answer is equal to correct or wrong answer
 // }
+function checkAnswer(){
+    $('ol').closest('li').on('click', function(){
+        console.log(`li clicked!`);
+    });
+}
 
 // function if correct/ wrong answer {
 //     if check answer is correct answer 
@@ -47,5 +79,10 @@ console.log('Script is connected!');
 // // function quiz controller {
 //     place all functions to run quiz here 
 // }
-
+function quizController(){
+    hideQuestions();
+    startQuiz();
+    checkAnswer();
+};
 // $(quiz controller);
+$(quizController);
