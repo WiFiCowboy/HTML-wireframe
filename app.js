d5c8b16c4ee5e924d14120f74be90db8ba446e22
Humberto Hernandez <hamburger45@gmail.com>
2019-11-01 18:29:52 -0700

    added app.js

---
+++ app.js
@@ -0,0 +1,51 @@
+console.log('Script is connected!');
+
+// const STORE = questions to render 
+// **hint check samples provided to see how they structuyred data 
+// ** else use and array object [{querstion: '?', answer: 'true || false'}]
+
+// Start the quiz 
+// function start quiz {
+//     listen for button click "start" and render quistions
+//  **Hint** try and use show and hide methods vs fully rendering 
+// }
+
+// function render score board {
+//     display score board with current score
+//     **this may not be DRY but validate before removing!!
+// }
+
+// function keep score {
+//     update score board based on correct or wrong answers 
+// }
+
+// function check answer {
+//     on button click 'submit' 
+//     listen for user answer 
+//     check if user answer is equal to correct or wrong answer
+// }
+
+// function if correct/ wrong answer {
+//     if check answer is correct answer 
+//     render correct answer page 
+//     if check answer is wrong answer 
+//     render wrong answer page
+// }
+
+// function track questions {
+//     keep track of questions 
+//     iterate through questions until false aka no more questions
+// }
+
+// function end of quiz {
+//     render end of quiz page
+//     display final Score 
+//     listen for try again and run start quiz () if clicked 
+//     NOTE** might want to add a quite quiz button 
+// }
+
+// // function quiz controller {
+//     place all functions to run quiz here 
+// }
+
+// $(quiz controller);
